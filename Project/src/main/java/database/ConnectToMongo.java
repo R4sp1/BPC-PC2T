cece1b538ac8630e57cc7c1a59125a1ed0c37f01
R4sp1 <57303554+R4sp1@users.noreply.github.com>
2022-04-22 21:19:24 +0200

    Connection to mongo is done using mongodb.uri defined in WM options

--- Project/src/main/java/database/ConnectToMongo.java
+++ Project/src/main/java/database/ConnectToMongo.java
@@ -13,7 +13,7 @@ public class ConnectToMongo {
 
 
     public static void ConnectToMongoDB() {
-        String connectionString = "mongodb+srv://BPC-PC2T-project:ppYd5tppGMa2Qjb@cluster0.v32md.mongodb.net/project?retryWrites=true&w=majority";
+        String connectionString = System.getProperty("mongodb.uri");
         try (MongoClient mongoClient = MongoClients.create(connectionString)) {
             List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
             databases.forEach(db -> System.out.println(db.toJson()));
